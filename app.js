58d9c83412909272e4215ce067c265b358c269a5
omenaka102 <93178465+omenaka102@users.noreply.github.com>
2023-03-16 16:04:48 +0100

    added Swagger docs

--- app.js
+++ app.js
@@ -9,14 +9,20 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// Swagger 
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 // connectDB
 const connectDB = require('./db/connect')
 const authenticateUser = require('./middleware/authentication')
 
 // IndexRoute 
 app.get('/', (req, res) => {
-  res.send("Jobs api")
+  res.send("<h1>Jobs API</h1><a href="/api-docs">Documentation</a>")
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routers
 const authRouter = require('./routes/auth')
